66a13456fdd35a4cf6f831aa0de8d91168e8b815
baegahyeon <1988_b@naver.com>
2020-09-04 23:02:37 +0900

    todo list setting

--- js/main.js
+++ js/main.js
@@ -6,12 +6,11 @@ const defalutElem = {
   nameInput: document.querySelector(".name__form input"),
   greeting: document.querySelector(".name__value"),
   todoForm: document.querySelector(".todo__form"),
-  todoInput:document.querySelector(".todo__form input")
+  todoInput: document.querySelector(".todo__form input"),
   todoList: document.querySelector(".todo__list"),
 };
-const USER_LS = "currentUser";
-const SHOWING_CN = "showing";
 
+//시간을 나타내는 함수
 function getTime() {
   const date = new Date();
   const hours = date.getHours();
@@ -22,6 +21,11 @@ function getTime() {
     minutes < 10 ? `0${minutes}` : minutes
   }:${seconds < 10 ? `0${seconds}` : seconds}`;
 }
+
+//이름을 설정하는 함수
+
+const USER_LS = "currentUser";
+const SHOWING_CN = "showing";
 
 function saveName(text) {
   localStorage.setItem(USER_LS, text);
@@ -54,8 +58,73 @@ function loadName() {
   }
 }
 
+//TODOLIST 함수
+
+const TODOS_LS = "toDos";
+
+let todoArray = [];
+
+function deleteTodo(e) {
+  e.preventDefault();
+  const btn = e.target;
+  const li = btn.parentNode;
+  defalutElem.todoList.removeChild(li);
+  const cleanTodos = todoArray.filter(function (todo) {
+    return todo.id !== parseInt(li.id);
+  });
+  todoArray = cleanTodos;
+  saveToDos();
+}
+
+function saveToDos() {
+  localStorage.setItem(TODOS_LS, JSON.stringify(todoArray));
+}
+
+function paintToDo(text) {
+  const li = document.createElement("li");
+  const delBtn = document.createElement("button");
+  delBtn.innerText = "X";
+  delBtn.addEventListener("click", deleteTodo);
+  const span = document.createElement("span");
+  span.innerText = text;
+  const newId = todoArray.length + 1;
+  li.appendChild(span);
+  li.appendChild(delBtn);
+  li.id = newId;
+  defalutElem.todoList.appendChild(li);
+  const todoObj = {
+    text: text,
+    id: newId,
+  };
+  todoArray.push(todoObj);
+  saveToDos();
+}
+
+function handleTodoSubmit(e) {
+  e.preventDefault();
+  const currentValue = defalutElem.todoInput.value;
+  paintToDo(currentValue);
+  defalutElem.todoInput.value = "";
+}
+
+function something(todoArray) {
+  console.log(todoArray);
+}
+
+function loadTodo() {
+  const currentList = localStorage.getItem(TODOS_LS);
+  if (currentList !== null) {
+    const parsedTodos = JSON.parse(currentList);
+    parsedTodos.forEach(function (todoArray) {
+      paintToDo(todoArray.text);
+    });
+  }
+  defalutElem.todoForm.addEventListener("submit", handleTodoSubmit);
+}
+
 addEventListener("load", () => {
   getTime();
   setInterval(getTime, 1000);
   loadName();
+  loadTodo();
 });
